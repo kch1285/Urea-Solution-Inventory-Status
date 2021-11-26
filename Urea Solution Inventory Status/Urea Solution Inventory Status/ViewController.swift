cde784ee5912e8e96d98a529e3d19460998e79c3
kch1285 <kch1285@naver.com>
2021-11-26 16:38:15 +0900

    Initial Commit

---
+++ Urea Solution Inventory Status/Urea Solution Inventory Status/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Urea Solution Inventory Status
+//
+//  Created by chihoooon on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
